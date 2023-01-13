9419839547558f7d5bf9d91f356aceaac8fe41e1
MelianMiko <melianmiko@yandex.ru>
2023-01-13 18:57:01 +0700

    Add pyproject.toml and requirements.txt

--- setup.py
+++ setup.py
@@ -1,17 +1,3 @@
 from setuptools import setup
 
-setup(
-    name='mmk_updater',
-    version='0.12.1',
-    packages=['mmk_updater'],
-    url='https://github.com/melianmiko/python-updater',
-    license='Apache 2.0',
-    install_requires=[],
-    extras_require={
-        "tk": ["tkinter"]
-    },
-    author='MelianMiko',
-    author_email='melianmiko@yandex.ru',
-    description='Updater tool',
-    include_package_data=True
-)
+setup()
